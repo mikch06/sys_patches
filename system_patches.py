98383f71ee480e2988b83b1f6837db84488bb273
mik <mik@interno.kissel.ch>
2024-01-28 15:47:42 +0100

    init system_patches.py with python config module

---
+++ system_patches.py
@@ -0,0 +1,89 @@
+import configparser
+import os
+import subprocess
+
+# Read host config
+#. config.cfg
+#config  = open('config.cfg', 'r')
+import config
+
+#print('Hosts: 0: all hosts', hosts)
+print('Hosts: 0: all hosts', config.hosts)
+config.select_host = int(input('Please select a host: '))
+
+while True:
+
+    print('Task: \n1 Ping \n2 Check open updates \n3 Append updates \n4 Reboot \n5 Quit')
+    select_task = int(input('Please select a task: '))
+
+    def check_ping():
+        if select_host == 0:
+            for value in config.hosts.values():
+                print('Ping check all hosts')
+                os.system("ping -c 1 " + value)
+                print(value)
+        else:
+            print('Ping check host')
+            os.system("ping -c 1 " + hosts[select_host])
+
+    def check_updates():
+        if config.select_host == 0:
+            print('Update check all hosts')
+            for value in config.hosts.values():
+                print("Check: ", value)
+                login = config.remote_user + '@' + value
+                subprocess.run(['ssh', login, 'dnf check-update'])
+                # subprocess.run(("ssh", "<REMOTE UNAME>@<REMOTE IP/HOSTNAME>", "free", "-m"))
+        else:
+            print('Update check host')
+            login = config.remote_user + '@' + hosts[select_host]
+            subprocess.run(['ssh', login, 'dnf check-update'])
+            #os.system("ping -c 1 " + hosts[select_host])
+
+    def install_updates():
+        if select_host == 0:
+            print('Install updates on all hosts')
+            for value in hosts.values():
+                print("Check: ", value)
+                login = remote_user + '@' + value
+                subprocess.run(['ssh', login, 'sudo dnf update -y'])
+        else:
+            print('Install updates on host')
+            login = remote_user + '@' + hosts[select_host]
+            subprocess.run(['ssh', login, 'sudo dnf update -y'])
+
+    def reboot_host():
+        if select_host == 0:
+            print('Just single host usage!')
+            pass
+        else:
+            print('Reboot host!')
+            login = remote_user + '@' + hosts[select_host]
+            subprocess.run(['ssh', login, 'sudo reboot'])
+
+    match select_task:
+        case 1:
+            print('Task: Ping check')
+            check_ping()
+
+        case 2:
+            print('Task: Update check')
+            check_updates()
+
+        case 3:
+            print('Task: Install updates')
+            install_updates()
+
+        case 4:
+            print('Task: Reboot host')
+            reboot_host()
+
+        case 5:
+            print("Bye!")
+            quit()
+
+        case _:
+            print("Choose an option.")
+
+
+
